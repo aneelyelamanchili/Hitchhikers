9b7d28fd2ca8be85bb623e9c9c2e96d89374a5c0
aneelyelamanchili <aneel@yelamanchili.io>
2017-04-05 19:11:21 -0700

    Removed socketIO

--- Hitchhikers/Pods/Target Support Files/Pods-Hitchhikers/Pods-Hitchhikers-frameworks.sh
+++ Hitchhikers/Pods/Target Support Files/Pods-Hitchhikers/Pods-Hitchhikers-frameworks.sh
@@ -90,12 +90,10 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/SlideMenuControllerSwift/SlideMenuControllerSwift.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Socket.IO-Client-Swift/SocketIO.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftIconFont/SwiftIconFont.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/SlideMenuControllerSwift/SlideMenuControllerSwift.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Socket.IO-Client-Swift/SocketIO.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftIconFont/SwiftIconFont.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
