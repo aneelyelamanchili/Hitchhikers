096589086dd0635b25f1d578531cdc0fde00ba0b
William Wang <wang975@usc.edu>
2017-04-05 21:02:05 -0700

    sign up view controller

---
+++ Hitchhikers/Hitchhikers/SignUpViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpViewController.swift
+//  Hitchhikers
+//
+//  Created by William Z Wang on 4/5/17.
+//  Copyright © 2017 William Z Wang. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
