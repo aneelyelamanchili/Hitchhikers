791ce7b302babcfc6d906deb4230fb99b6ea1e81
davidsealand <sealand@usc.edu>
2017-04-19 20:43:59 -0700

    functionality

--- HitchhikersBackend/src/hitchhikers/WebSocketEndpoint.java
+++ HitchhikersBackend/src/hitchhikers/WebSocketEndpoint.java
@@ -6,7 +6,9 @@ import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Level;
@@ -30,7 +32,6 @@ import org.json.JSONObject;
 public class WebSocketEndpoint {
 	private static final Logger logger = Logger.getLogger("BotEndpoint");
 	private static final Map<String, Session> sessions = new HashMap<String, Session>();
-//	private static final Map<String, Session> usernames = new HashMap<>();
 	private static Application application = new Application();
 	private static Lock lock = new ReentrantLock();
 
@@ -73,10 +74,12 @@ public class WebSocketEndpoint {
 		lock.lock();
 		logger.log(Level.INFO, "Connection closed. (id:)" + session.getId());
 		sessions.remove(session.getId());
-//		if (factories.get(session.getId()) != null) {
-//			factories.get(session.getId()).killWorkers();
-//			factories.remove(session.getId());
-//		}
+		if (sessions.get(session.getId()) != null) {
+//			sess.get(session.getId()).killWorkers();
+			application.removeSession(session);
+			sessions.remove(session.getId());
+			
+		}
 		lock.unlock();
 	}
 
@@ -97,8 +100,33 @@ public class WebSocketEndpoint {
 		}
 		lock.unlock();
 	}
-	public void sendToSessions(ArrayList<Session> sendTo, String message) {
+	public void sendToSession(String email, byte[] message) {
 		lock.lock();
+		try {
+			ByteBuffer b = ByteBuffer.wrap(message);
+			System.out.println(email);
+			if (application.getSession(email) != null) {
+				System.out.println("got the session");
+				application.getSession(email).getBasicRemote().sendBinary(b);
+			}
+		} catch (IOException ex) {
+			sessions.remove(application.getSession(email).getId());
+			logger.log(Level.SEVERE, ex.getMessage(), ex.getStackTrace());
+		}
+		lock.unlock();
+	}
+	public void sendToSessions(Session session, byte[] message) {
+		lock.lock();
+		try {
+			ByteBuffer b = ByteBuffer.wrap(message);
+			for (String key : sessions.keySet()) {
+			    if (sessions.get(key)!=session) {
+			    	sessions.get(key).getBasicRemote().sendBinary(b);
+			    }
+			}
+		} catch (IOException x) {
+			x.printStackTrace();
+		}
 		lock.unlock();
 	}
 }
--- database/Hitchhikers.sql
+++ database/Hitchhikers.sql
@@ -30,11 +30,11 @@ CREATE TABLE `TotalPreviousTrips` (
     `CarModel` VARCHAR(50) NOT NULL,
     `LicensePlate` VARCHAR(50) NOT NULL,
     `Cost` INT(11) NOT NULL, 
-    `Date/Time` VARCHAR(50) NOT NULL,
-    `Detours` VARCHAR(50) NOT NULL,
-    `Hospitality` VARCHAR(100) NOT NULL,
-    `Food` VARCHAR(100) NOT NULL,
-    `Luggage` VARCHAR(100) NOT NULL,
+    `Date/Time` VARCHAR(100) NOT NULL,
+    `Detours` VARCHAR(200) NOT NULL,
+    `Hospitality` VARCHAR(200) NOT NULL,
+    `Food` VARCHAR(200) NOT NULL,
+    `Luggage` VARCHAR(200) NOT NULL,
     `TotalSeats` INT(11) NOT NULL,
     `SeatsFilled` INT(11) NOT NULL,
     FOREIGN KEY(`userID`) REFERENCES `TotalUsers`(`userID`)
@@ -51,11 +51,11 @@ CREATE TABLE `CurrentTrips` (
     `CarModel` VARCHAR(50) NOT NULL,
     `LicensePlate` VARCHAR(50) NOT NULL,
     `Cost` INT(11) NOT NULL, 
-    `Date/Time` VARCHAR(50) NOT NULL,
-    `Detours` VARCHAR(50) NOT NULL,
-    `Hospitality` VARCHAR(100) NOT NULL,
-    `Food` VARCHAR(100) NOT NULL,
-    `Luggage` VARCHAR(100) NOT NULL,
+    `Date/Time` VARCHAR(100) NOT NULL,
+    `Detours` VARCHAR(200) NOT NULL,
+    `Hospitality` VARCHAR(200) NOT NULL,
+    `Food` VARCHAR(200) NOT NULL,
+    `Luggage` VARCHAR(200) NOT NULL,
     `TotalSeats` INT(11) NOT NULL,
     `SeatsAvailable` INT(11) NOT NULL,
     FOREIGN KEY(`userID`) REFERENCES `TotalUsers`(`userID`),
@@ -64,7 +64,7 @@ CREATE TABLE `CurrentTrips` (
 
 INSERT INTO `TotalUsers` (FirstName, LastName, Password, Email, `Age`, `PhoneNumber`, `Picture`) VALUES ('Aneel', 'Yelamanchili', 'aneel', 'ayelaman@usc.edu', 20, '5039169169', 'https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/13507226_1148683701850248_1676098310529753614_n.jpg?oh=8fc80ae2dd67fa09b7b8e40c801c8130&oe=5951E6F9');
 INSERT INTO `TotalUsers` (FirstName, LastName, Password, Email, `Age`, `PhoneNumber`, `Picture`) VALUES ('David', 'Sealand', 'david', 'sealand@usc.edu', 19, '4919193020', 'https://scontent-lax3-2.xx.fbcdn.net/v/t31.0-8/12841330_663274557146113_2810161329870379623_o.jpg?oh=98a2db28fbd7e4df98aaa19f3f6c9bc6&oe=59958317');
-INSERT INTO `TotalUsers` (FirstName, LastName, Password, Email, `Age`, `PhoneNumber`, `Picture`) VALUES ('Adam', 'Espinoza', 'adam', 'adamespi@usc.edu', 20, '9101223112', 'https://scontent-lax3-2.xx.fbcdn.net/v/t31.0-8/13147448_1342907809059162_3392165128965960124_o.jpg?oh=ecd5ccd6b6b137c91f1b3491563da338&oe=597E9B31');
+INSERT INTO `TotalUsers` (FirstName, LastName, Password, Email, `Age`, `PhoneNumber`, `Picture`) VALUES ('Adam', 'Espinoza', 'adam', 'adamespi@usc.edu', 20, '9101223112', 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAiDAAAAJDNiMWE3YTkyLTRiMzAtNDJkNS1hMjE3LWE2ZWFiNDlkMDgwNA.jpg');
 INSERT INTO `TotalUsers` (FirstName, LastName, Password, Email, `Age`, `PhoneNumber`, `Picture`) VALUES ('William', 'Wang', 'william', 'wang975@usc.edu', 20, '8083219909', 'https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/12122921_974493999258444_5174282398401993915_n.jpg?oh=33da5dee348b806e9d7554f9750a3a5b&oe=59961E92');
 INSERT INTO `TotalPreviousTrips`( rideID, userID,FirstName, LastName,Email,StartingPoint, DestinationPoint, CarModel, LicensePlate, Cost, `Date/Time`, Detours, Hospitality, Food, Luggage, TotalSeats, SeatsFilled) VALUES (1,1,'Adam','Espinoza', 'adamespi@usc.edu','San Fracisco,CA', 'Los Angeles', 'Honda Civic' , '64j74k', 25, 'Wednesday April 19', 'no detours', 'bring pillows', 'i have snacks', 'no luggage space', 4, 2);
 INSERT INTO `CurrentTrips`( userID,FirstName,LastName,Email,StartingPoint, DestinationPoint, CarModel, LicensePlate, Cost, `Date/Time`, Detours, Hospitality, Food, Luggage, TotalSeats, SeatsAvailable) VALUES (2,'David','Sealand', 'sealand@usc.edu','Los Angeles, CA, USA', 'Portland, OR, USA', 'Bugatti Veyron' , '202JEJ', 20, 'Friday April 21', 'no detours', 'I love food', 'i have no snacks', 'lots of luggage space', 2, 1);
